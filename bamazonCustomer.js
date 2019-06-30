e2a0c2bc64c030b0240459702f5dddbb361d08ba
Vincent Romine <vinjrom@live.com>
2019-06-30 17:42:51 -0500

    added stock reduction function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,49 +15,71 @@ connection.connect(function(err) {
     start();
 });
 
+// asks if user would like to buy item
+var start = function() {
+    inquirer.prompt({
+        name: "yesorno",
+        type: "rawlist",
+        message: "Would you like to buy an item?",
+        choices: ["Yes", "No"],
+    }).then(function(answer) {
+        if (answer.yesorno == "Yes") {
+            buyFunction();
+        } else {
+            start();
+        }
+    })
+}
+
 // user picks from list of items to buy
-var buyingList = function() {
-        connection.query("SELECT * FROM products", function(err, res) {
-            console.log(res);
-            inquirer.prompt({
-                name: "choice",
-                type: "rawlist",
-                // forloop displays products as array-objects
-                choices: function(value) { var productsArray = []; for (var i = 0; i < res.length; i++) { productsArray.push(res[i].item_name); } return choiceArray; },
-                message: "What would you like to buy?"
-                    // calls product items,
-            }).then(function(answer) {
+var buyFunction = function() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        console.log(res);
+        inquirer.prompt({
+            name: "choice",
+            type: "rawlist",
+            // forloop displays products as array-objects
+            choices: function(value) {
+                var choiceArray = [];
                 for (var i = 0; i < res.length; i++) {
-                    if (res[i].item_name == answer.choice) {
-                        var chosenItem = res[i];
-                        inquirer.prompt({
-                            name: "qty",
-                            type: "input",
-                            message: "How many would you like to buy?",
-                            validate: function(value) {
-                                    if (isNaN(value) == false) {
-                                        return true;
-                                    } else {
-                                        return false;
-                                    }
-                                }
-                                // checks if answer is lower than product quantity
-                        }).then(function(answer) {
-                            if (chosenItem.stock_quantity < parseInt(answer.qty)) {
-                                connection.query("UPDATE auctions SET ? WHERE ?", [{
-                                    stock_quantity: answer.qty
-                                }, {
-                                    id: chosenItem.id
-                                }], function(err, res) {
-                                    console.log("Bid successfully placed!\n");
-                                    start();
-                                });
-                            } else {
-                                console.log("\nYour qty was too low. Try again...");
-                                start();
-                            }
-                        })
-                    }
+                    choiceArray.push(res[i].item_name);
                 }
-            })
+                return choiceArray;
+            },
+            message: "What would you like to buy?"
+                // calls product items,
+        }).then(function(answer) {
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].item_name == answer.choice) {
+                    var chosenItem = res[i];
+                    inquirer.prompt({
+                        name: "qty",
+                        type: "input",
+                        message: "How many would you like to buy?",
+                        // verifies numerical quantity
+                        validate: function(value) {
+                                if (isNaN(value) == false) {
+                                    return true;
+                                } else {
+                                    return false;
+                                }
+                            }
+                            // checks if answer is lower than product quantity
+                    }).then(function(answer) {
+                        if (chosenItem.stock_quantity > parseInt(answer.qty)) {
+
+                            // if answer is less than stock quantity, reduce stock quantity
+                            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + answer.qty + " WHERE id = " + chosenItem.id);
+                            console.log("Item successfully ordered!");
+                            start();
+                            // console.log("New " + chosenItem.item_name + " qty = " + chosenItem.stock_quantity)
+                        } else {
+                            console.log("Sorry, we are out of stock for that item.");
+                            start();
+                        }
+                    })
+                }
+            }
         })
+    })
+}
---
+++ start-products.sql
@@ -0,0 +1,19 @@
+USE bamazon_db;
+
+INSERT INTO products
+VALUES (01, "Alienware desktop", "electronics", 895,2000);
+
+INSERT INTO products
+VALUES (02, 'Razor mechanical-keyboard', "electronics", 95,2200);
+
+INSERT INTO products
+VALUES (03, '30" monitor', "electronics", 60,1500);
+
+INSERT INTO products
+VALUES (04, 'Razor mechanical-keyboard', "electronics", 95,2000);
+
+INSERT INTO products
+VALUES (05, 'desk', "furniture", 45,1450);
+
+INSERT INTO products
+VALUES (06, 'desk chair', "furniture", 35,1450);
